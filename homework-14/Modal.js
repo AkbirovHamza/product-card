367b2a2b4af54851980499fe8d6b0fe385d9a6c9
h9574758-cyber <h9574758@gmail.com>
2026-09-11 13:53:04 +0300

    (Homework 14)fix errors with modal

--- homework-14/Modal.js
+++ homework-14/Modal.js
@@ -1,58 +1,51 @@
 export class Modal {
+
   constructor(modalId, buttonId) {
+
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById('overlay');
-    this.closeHandler = null;
-    this.overlayHandler = null;
+
+    this.closeBtn = this.modal.querySelector('.modal-close-button');
+
+    this.handler = () => {
+      this.close();
+    };
+
     this.#initOpen(buttonId);
   }
 
   open() {
+
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
+
     this.#initClose();
   }
 
   close() {
+
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-
-    const closeBtn = this.modal.querySelector('.modal-close-button');
-    closeBtn.removeEventListener('click', this.closeHandler);
-
-    this.overlay.removeEventListener('click', this.overlayHandler);
-
-    this.closeHandler = null;
-    this.overlayHandler = null;
   }
 
   isOpen() {
+
     return this.modal.classList.contains('modal-showed');
   }
 
   #initClose() {
-    const closeBtn = this.modal.querySelector('.modal-close-button');
 
-    this.closeHandler = () => {
-      this.close();
-    };
-    closeBtn.addEventListener('click', this.closeHandler);
-
-    this.overlayHandler = () => {
-      document.querySelectorAll('.modal-showed').forEach(modal => {
-        modal.classList.remove('modal-showed');
-      });
-      this.overlay.classList.remove('overlay-showed');
-    };
-    this.overlay.addEventListener('click', this.overlayHandler);
+    this.closeBtn.addEventListener('click', this.handler);
+    this.overlay.addEventListener('click', this.handler);
   }
 
   #initOpen(buttonId) {
+
     const openButton = document.getElementById(buttonId);
-    if (openButton) {
-      openButton.addEventListener('click', () => {
-        this.open();
-      });
-    }
+
+    openButton.addEventListener('click', () => {
+      this.open();
+    });
   }
+
 }
